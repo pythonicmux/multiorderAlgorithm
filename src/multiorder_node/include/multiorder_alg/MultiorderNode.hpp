c613fdcdd7aeb0693ad80864933c1a8770a4cbf8
pythonicmux <michaelli1234@DESKTOP-516GN9G.localdomain>
2021-02-22 22:29:39 -0500

    Precompute distances between all pairs of nodes

--- src/multiorder_node/include/multiorder_alg/MultiorderNode.hpp
+++ src/multiorder_node/include/multiorder_alg/MultiorderNode.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <limits>
 #include <map>
 #include <utility>
 #include <vector>
@@ -13,10 +14,24 @@ public:
     virtual ~MultiorderNode();
 
 private:
-    static const std::vector<char> nodes_; 
     ros::NodeHandle& nh_;
-    std::map<char, std::set<char>> edges_;
-    std::map<std::pair<char, char>, double> weights_;
+
+    static constexpr int numNodes_ = 7;
+    
+    // This has the neighbors of each node. A node has a neighbor iff 
+    // the node and neighbor are connected directly by a road. 
+    std::map<int, std::set<int>> neighbors_;
+    // The weight represents the seconds it takes to traverse an edge, 
+    // assuming the robot travels at 1 m/s. Because it's 1 m/s, it also 
+    // represents the distance in meters between the vertices of the edge.
+    double weights_[numNodes_][numNodes_];
+
+    // This maps each pair of points to the minimum travel time 
+    // between them (i.e. the cost of the lowest-cost path between them).
+    double minTravelTimes_[numNodes_][numNodes_];
+    // This will calculate minTravelTimes_ with the pairwise minimum 
+    // travel time between each pair of nodes.
+    void precalculateMinTravelTimes();
 };
 
 } // namespace Multiorder
--- src/multiorder_node/src/MultiorderNode.cpp
+++ src/multiorder_node/src/MultiorderNode.cpp
@@ -5,70 +5,134 @@ namespace Multiorder {
 /*
  * The graph of engineering quad roads is as follows: (weights are distances in meters)
  * Wean Turtle             Doherty Main Entrance
- * A--89.62--B---38.74---C---53.97---D
+ * 0--89.62--1---38.74---2---53.97---3
  * |         |        /  |
  * 32.29    31.68 49.62 34.7           
  * |         |     /     |
- * E--91.91--F---39.59---G
+ * 4--91.91--5---39.59---6
  * Porter               Baker
  */
-const std::vector<char> MultiorderNode::nodes_({'A', 'B', 'C', 'D', 'E', 'F', 'G'});
-
 MultiorderNode::MultiorderNode(ros::NodeHandle& nodeHandle) : nh_(nodeHandle) {
     // Initialize the graph.
-    for(char node:nodes_) {
-        edges_[node] = std::set<char>{};
-        for(char nbr:nodes_) {
-            if(node != nbr) {
-                weights_[std::make_pair(node, nbr)] = -1.0;
+    for(int i = 0; i < numNodes_; i++) {
+        neighbors_[i] = std::set<int>{};
+        for(int j = 0; j < numNodes_; j++) {
+            if(i != j) {
+                weights_[i][j] = -1.0;
+            } else {
+                weights_[i][j] = 0.0;
             }
         }
     }
 
-    edges_['A'].insert('B');
-    weights_[std::pair<char, char>('A', 'B')] = 89.62;
-    edges_['A'].insert('E');
-    weights_[std::pair<char, char>('A', 'E')] = 91.91;
+    neighbors_[0].insert(1);
+    weights_[0][1] = 89.62;
+    neighbors_[0].insert(4);
+    weights_[0][4] = 91.91;
 
-    edges_['B'].insert('A');
-    weights_[std::pair<char, char>('B', 'A')] = 89.62;
-    edges_['B'].insert('C');
-    weights_[std::pair<char, char>('B', 'C')] = 38.74;
-    edges_['B'].insert('F');
-    weights_[std::pair<char, char>('B', 'F')] = 31.68;
+    neighbors_[1].insert(0);
+    weights_[1][0] = 89.62;
+    neighbors_[1].insert(2);
+    weights_[1][2] = 38.74;
+    neighbors_[1].insert(5);
+    weights_[1][5] = 31.68;
 
-    edges_['C'].insert('B');
-    weights_[std::pair<char, char>('C', 'B')] = 38.74;
-    edges_['C'].insert('D');
-    weights_[std::pair<char, char>('C', 'D')] = 53.97;
-    edges_['C'].insert('F');
-    weights_[std::pair<char, char>('C', 'F')] = 49.62;
-    edges_['C'].insert('G');
-    weights_[std::pair<char, char>('C', 'G')] = 34.7;
+    neighbors_[2].insert(1);
+    weights_[2][1] = 38.74;
+    neighbors_[2].insert(3);
+    weights_[2][3] = 53.97;
+    neighbors_[2].insert(5);
+    weights_[2][5] = 49.62;
+    neighbors_[2].insert(6);
+    weights_[2][6] = 34.7;
 
-    edges_['D'].insert('C');
-    weights_[std::pair<char, char>('D', 'C')] = 53.97;
+    neighbors_[3].insert(2);
+    weights_[3][2] = 53.97;
 
-    edges_['E'].insert('A');
-    weights_[std::pair<char, char>('E', 'A')] = 32.29;
-    edges_['E'].insert('F');
-    weights_[std::pair<char, char>('E', 'F')] = 91.91;
+    neighbors_[4].insert(0);
+    weights_[4][0] = 32.29;
+    neighbors_[4].insert(5);
+    weights_[4][5] = 91.91;
 
-    edges_['F'].insert('B');
-    weights_[std::pair<char, char>('F', 'B')] = 31.68;
-    edges_['F'].insert('C');
-    weights_[std::pair<char, char>('F', 'C')] = 49.62;
-    edges_['F'].insert('E');
-    weights_[std::pair<char, char>('F', 'E')] = 91.91;
-    edges_['F'].insert('G');
-    weights_[std::pair<char, char>('F', 'G')] = 39.59;
+    neighbors_[5].insert(1);
+    weights_[5][1] = 31.68;
+    neighbors_[5].insert(2);
+    weights_[5][2] = 49.62;
+    neighbors_[5].insert(4);
+    weights_[5][4] = 91.91;
+    neighbors_[5].insert(6);
+    weights_[5][6] = 39.59;
 
-    edges_['G'].insert('C');
-    weights_[std::pair<char, char>('G', 'C')] = 34.7;
-    edges_['G'].insert('C');
-    weights_[std::pair<char, char>('G', 'C')] = 39.59;
+    neighbors_[6].insert(2);
+    weights_[6][2] = 34.7;
+    neighbors_[6].insert(2);
+    weights_[6][2] = 39.59;
+
+    precalculateMinTravelTimes();
+
+    // Make sure the precomputations are correct. 
+    for (int i = 0; i < numNodes_; i++) {
+        ROS_INFO_STREAM(i << " is connected to: ");
+        for (int j : neighbors_[i]) {
+            ROS_INFO_STREAM(j << " ");
+        }
+        ROS_INFO_STREAM('\n');
+    }
+ 
+    for (int i = 0; i < numNodes_; i++) {
+        for (int j = 0; j < numNodes_; j++) {
+            ROS_INFO_STREAM("d(" << i << ", " << j << ") = " << minTravelTimes_[i][j] << "\n");
+        }
+    }
 }
 
 MultiorderNode::~MultiorderNode() {}
 
+void MultiorderNode::precalculateMinTravelTimes() {
+    // Calculate the min travel times from this node 
+    // with Dijkstra's algorithm.
+    for(int source = 0; source < numNodes_; source++) {
+        // Each node only needs to be smoothed once, 
+        // so keep track of done nodes.
+        std::vector<bool> done(numNodes_, false);
+
+        for(int i = 0; i < numNodes_; i++) {
+            minTravelTimes_[source][i] = DBL_MAX;
+        }
+        minTravelTimes_[source][source] = 0;
+
+        for (int i = 0; i < numNodes_; i++) {
+            // Get the closest node to the source node for smoothing.
+            int closest = -1;
+            for(int j = 0; j < numNodes_; j++) {
+                if (!done[j] && (closest == -1 || 
+                    minTravelTimes_[source][j] < 
+                    minTravelTimes_[source][closest])) {
+                    closest = j;
+                }
+            }
+
+            // Only inaccessible nodes left if nothing is reachable anymore.
+            if (minTravelTimes_[source][closest] == DBL_MAX) {
+                break;
+            }
+
+            // Smooth out this node to make sure it's the true min travel time.
+            done[closest] = true;
+            for(int nbr:neighbors_[closest]) {
+                // nbr gets "smoothed" if there's a faster 
+                // path to the node via source -> ... -> closest -> nbr.
+                if (minTravelTimes_[source][closest] + weights_[closest][nbr] < 
+                        minTravelTimes_[source][nbr]) {
+                    minTravelTimes_[source][nbr] = minTravelTimes_[source][closest] + 
+                        weights_[closest][nbr];
+                }
+
+                if (weights_[closest][nbr] < 0.0) {
+                    ROS_ERROR("Dijkstra's found that a neighbor has negative weight.");
+                }
+            } 
+        }
+    }
+}
 } // namespace Multiorder
